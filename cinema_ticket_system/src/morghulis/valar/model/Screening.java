65d2d272b8575adf6e4d3c236fe3cc8e5f0bc919
svilen <svilenkomitov@gmail.com>
2015-05-19 15:42:56 +0300

    change Screening

--- cinema_ticket_system/src/morghulis/valar/model/Screening.java
+++ cinema_ticket_system/src/morghulis/valar/model/Screening.java
@@ -57,13 +57,13 @@ public class Screening implements Serializable {
 		this.movie = movie;
 	}
 
+	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((hall == null) ? 0 : hall.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((movie == null) ? 0 : movie.hashCode());
 		return result;
 	}
 
@@ -76,31 +76,19 @@ public class Screening implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Screening other = (Screening) obj;
-		if (hall == null) {
-			if (other.hall != null)
-				return false;
-		} else if (!hall.equals(other.hall))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (movie == null) {
-			if (other.movie != null)
-				return false;
-		} else if (!movie.equals(other.movie))
 			return false;
 		return true;
 	}
 
 	@Override
 	public String toString() {
-		return "Screening [id=" + id + ", hall=" + hall + ", movie=" + movie
-				+ "]";
+		return "Screening [hall=" + hall + ", movie=" + movie + "]";
 	}
-	
-	
+
 	
 	
 }
