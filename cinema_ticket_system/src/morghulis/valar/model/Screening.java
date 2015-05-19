ba92a39830f4f35f9137c59c115b4eddf0db357c
svilen <svilenkomitov@gmail.com>
2015-05-19 16:16:46 +0300

    add Screening entity

---
+++ cinema_ticket_system/src/morghulis/valar/model/Screening.java
@@ -0,0 +1,12 @@
+package morghulis.valar.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Screening implements Serializable {
+
+	private static final long serialVersionUID = 2520912188484284800L;
+
+}
