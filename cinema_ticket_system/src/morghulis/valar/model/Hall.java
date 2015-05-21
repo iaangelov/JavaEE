0acddb989f4534cf38e64f2ae0b1878f3d87da31
svilen <svilenkomitov@gmail.com>
2015-05-21 12:36:19 +0300

    add ScreeningDAO and Screening

--- cinema_ticket_system/src/morghulis/valar/model/Hall.java
+++ cinema_ticket_system/src/morghulis/valar/model/Hall.java
@@ -2,4 +2,5 @@ package morghulis.valar.model;
 
 public class Hall {
 
+	
 }
--- cinema_ticket_system/src/morghulis/valar/model/Screening.java
+++ cinema_ticket_system/src/morghulis/valar/model/Screening.java
@@ -1,13 +1,24 @@
 package morghulis.valar.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
+@NamedQueries({
+	@NamedQuery(name = "allScreenings", query = "SELECT s FROM Screening s"),
+	@NamedQuery(name = "allScreeningsByHallNumber", query = "SELECT s FROM Screening s WHERE s.hall.hallNUmber = :hallNumber")
+})
 public class Screening implements Serializable {
 
 	private static final long serialVersionUID = 2520912188484284800L;
@@ -16,8 +27,14 @@ public class Screening implements Serializable {
 	@GeneratedValue (strategy = GenerationType.AUTO)
 	private Long id;
 	
+	@Temporal(TemporalType.DATE)
+	private Date screeningDate;
+	@ManyToOne
 	private Hall hall;
+	
+	@OneToOne
 	private Movie movie;
+	
 	public Screening() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -32,6 +49,13 @@ public class Screening implements Serializable {
 	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	
+	public Date getScreeningDate() {
+		return screeningDate;
+	}
+	public void setScreeningDate(Date screeningDate) {
+		this.screeningDate = screeningDate;
 	}
 	public Hall getHall() {
 		return hall;
