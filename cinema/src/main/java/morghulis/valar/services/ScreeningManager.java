057348ffe606ab4f7504a790be0a286a00f5149f
svilen <svilenkomitov@gmail.com>
2015-05-22 15:07:17 +0300

    add Screening manager

---
+++ cinema/src/main/java/morghulis/valar/services/ScreeningManager.java
@@ -0,0 +1,27 @@
+package morghulis.valar.services;
+
+import java.util.Collection;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import morghulis.valar.dao.ScreeningDAO;
+import morghulis.valar.model.Screening;
+
+@Stateless
+@Path("screening")
+public class ScreeningManager {
+
+	@EJB
+	private ScreeningDAO screeningDAO;
+	
+	@GET
+	@Produces("application/json")
+	public Collection<Screening> getAllScreenings() {
+		
+		return screeningDAO.getAllScreenings();
+	}
+}
