1ce0659ad0f37e63211af1159b80c8b995bbaeb5
svilen <svilenkomitov@gmail.com>
2015-05-23 11:29:47 +0300

    add getScreeningById

--- cinema/src/main/java/morghulis/valar/services/ScreeningManager.java
+++ cinema/src/main/java/morghulis/valar/services/ScreeningManager.java
@@ -6,6 +6,7 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import morghulis.valar.dao.ScreeningDAO;
@@ -23,5 +24,13 @@ public class ScreeningManager {
 	public Collection<Screening> getAllScreenings() {
 		
 		return screeningDAO.getAllScreenings();
+	}
+	
+	@GET
+	@Path("{screeningId}")
+	@Produces("application/json")
+	public Screening getScreeningById(@PathParam("screeningId") String screeningId) {
+		
+		return screeningDAO.findScreeningById(Long.parseLong(screeningId));
 	}
 }
