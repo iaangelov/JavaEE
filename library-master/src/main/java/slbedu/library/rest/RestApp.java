91282bd29cd74cf3c90f0ba47c4da480d5a2e4c5
KirilKabakchiev <niki_kab@abv.bg>
2015-05-26 17:21:27 +0300

    fix2

--- library-master/src/main/java/slbedu/library/rest/RestApp.java
+++
@@ -1,9 +0,0 @@
-package slbedu.library.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class RestApp extends Application {
-
-}
