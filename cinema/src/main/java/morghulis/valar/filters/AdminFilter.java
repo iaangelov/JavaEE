22f8574d5629582cb00ec6229aef0c944cd9d2dd
dpanayotov <dpanayotov94@gmail.com>
2015-06-13 14:58:29 +0300

    add filter, beautify login & register

---
+++ cinema/src/main/java/morghulis/valar/filters/AdminFilter.java
@@ -0,0 +1,53 @@
+package morghulis.valar.filters;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import morghulis.valar.services.UserContext;
+import morghulis.valar.utils.UserType;
+
+@WebFilter(urlPatterns = { "/rest/admin/*", "/adminPanel.html" })
+public class AdminFilter implements Filter {
+
+	@Inject
+	private UserContext userContext;
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		if (userContext.getCurrentUser() != null) {
+			UserType type = userContext.getCurrentUser().getUserType();
+			if (type != null && type == UserType.ADMINISTRATOR) {
+				chain.doFilter(request, response);
+				return;
+			}
+		}
+
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		String redirectURL = httpServletRequest.getContextPath()
+				+ "/index.html";
+		httpServletResponse.sendRedirect(redirectURL);
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+}
--- cinema/src/main/webapp/js/functions.js
+++ cinema/src/main/webapp/js/functions.js
@@ -9,7 +9,7 @@ function loginControls() {
 				$(".logout").css("display", "inline");
 				isUserAuthenticated = true;
 			},
-			401: function() {
+			401 : function() {
 				$(".login_register").css("display", "inline");
 			}
 		}
@@ -26,14 +26,108 @@ function getUserName() {
 		if (typeof data != 'undefined') {
 			$(".welcome-greeting").css("display", "inline");
 			$("#user-holder").text(data);
-		} 
+		}
 	});
 }
 
+//show error and success messages on the index page 
+function showMessage(ElementID) {
+	$(ElementID).css({
+		'width' : '40%',
+		'margin-top' : '4.5%',
+		'text-align' : 'center',
+		'margin-left' : '30%'
+	});
+	$(ElementID).show();
+	localStorage.clear();
+}
 
-function indexInit(){
+function indexInit() {
 	getUserName();
 	loginControls();
+}
+
+function register() {
+	var username = $("#usernameRegister")[0].value;
+	var password = $("#pwdRegister")[0].value;
+	var email = $("#email")[0].value;
+
+	if (!isPasswordValid(password)) {
+		return;
+	}
+
+	var data = {
+		user : {
+			username : username,
+			password : password,
+			email : email
+		}
+	}
+
+	$.ajax({
+		url : 'rest/user',
+		type : "POST",
+		async: false,
+		contentType : "application/json;charset=UTF-8",
+		data : JSON.stringify(data)
+	}).success(function(data) {
+		localStorage.setItem('registerSuccess', 'true');
+		return;
+	}).fail(function(data) {
+		localStorage.setItem('userTaken', 'true');
+		return;
+	});
+}
+
+function isPasswordValid(password) {
+	var confirmPassword = $("#conf_pwdRegister")[0].value;
+
+	if (password == "" || confirmPassword == "") {
+		alert("Please fill both fields for password and try again.");
+		return false;
+	}
+
+	if (password != confirmPassword) {
+		alert("Two passwords do not match. Please correct the values and try again.");
+		return false;
+	}
+	return true;
+}
+
+function login() {
+	var username = $("#usernameLogin")[0].value;
+	var password = $("#pwdLogin")[0].value;
+
+	if (username == "" || password == "") {
+		alert("Username and password should not be empty.");
+		return;
+	}
+
+	var data = {
+		user : {
+			username : username,
+			password : password
+		}
+	}
+
+	$.ajax({
+		url : 'rest/user/login',
+		type : "POST",
+		async: false,
+		contentType : "application/json;charset=UTF-8",
+		data : JSON.stringify(data),
+		statusCode : {
+			401 : function() {
+				localStorage.setItem('loginFail', 'true');
+				return;
+			},
+			200 : function() {
+				localStorage.setItem('loginSuccess', 'true');
+				window.location.replace("index.html");
+				return;
+			}
+		}
+	});
 }
 
 function logout() {
